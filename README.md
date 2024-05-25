cb4ffe510dd59b36b35c0cf7b3ce7e9b0c4e6814
Nirav <niravpatelp129@gmail.com>
2024-05-25 19:21:26 -0400

    * feat: added .gitignore, README.md, package.json, and src/index.js

---
+++ README.md
@@ -0,0 +1,36 @@
+# React Glass Zoom
+
+React Glass Zoom is a React component that provides a magnification effect on images, similar to a magnifying glass. This component is highly customizable and easy to integrate into your React applications.
+
+## Features
+
+- Simple and easy to use
+- Highly customizable magnifier size and zoom level
+- No dependencies other than React
+
+## Installation
+
+Install the package via npm:
+
+`npm i react-glass-zoom`
+
+## Usage
+
+javascript
+
+```
+  <MagnifierWrapper>
+                <img
+                  src="https://placehold.co/600x400/EEE/31343C"
+                  alt='Front'
+                />
+              </MagnifierWrapper>
+```
+
+## Licence
+
+MIT
+
+## Keywords
+
+none
---
+++ src/index.js
@@ -0,0 +1,73 @@
+// src/index.js
+import { useRef, useState } from 'react';
+
+const MagnifierWrapper = ({
+  children,
+  width,
+  height,
+  magnifierHeight = 200,
+  magnifieWidth = 200,
+  zoomLevel = 3,
+}) => {
+  const [[x, y], setXY] = useState([0, 0]);
+  const [[imgWidth, imgHeight], setSize] = useState([0, 0]);
+  const [showMagnifier, setShowMagnifier] = useState(false);
+  const imageRef = useRef(null);
+
+  return (
+    <div
+      style={{
+        position: 'relative',
+        height: height,
+        width: width,
+      }}
+    >
+      <img
+        src={children.props.src}
+        style={{ height: height, width: width }}
+        ref={imageRef}
+        onMouseEnter={(e) => {
+          const elem = e.currentTarget;
+          const { width, height } = elem.getBoundingClientRect();
+          setSize([width, height]);
+          setShowMagnifier(true);
+        }}
+        onMouseMove={(e) => {
+          const elem = e.currentTarget;
+          const { top, left } = elem.getBoundingClientRect();
+
+          const x = e.pageX - left - window.pageXOffset;
+          const y = e.pageY - top - window.pageYOffset;
+          setXY([x, y]);
+        }}
+        onMouseLeave={() => {
+          setShowMagnifier(false);
+        }}
+        alt={'img'}
+      />
+
+      <div
+        style={{
+          display: showMagnifier ? '' : 'none',
+          position: 'absolute',
+
+          pointerEvents: 'none',
+          height: `${magnifierHeight}px`,
+          width: `${magnifieWidth}px`,
+          top: `${y - magnifierHeight / 2}px`,
+          left: `${x - magnifieWidth / 2}px`,
+          opacity: '1',
+          border: '1px solid lightgray',
+          backgroundColor: 'white',
+          backgroundImage: `url('${children.props.src}')`,
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: `${imgWidth * zoomLevel}px ${imgHeight * zoomLevel}px`,
+          backgroundPositionX: `${-x * zoomLevel + magnifieWidth / 2}px`,
+          backgroundPositionY: `${-y * zoomLevel + magnifierHeight / 2}px`,
+        }}
+      ></div>
+    </div>
+  );
+};
+
+export default MagnifierWrapper;
