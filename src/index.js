69f127dc23e2724336c5d292806eb7c8172d2a52
Nirav <niravpatelp129@gmail.com>
2024-05-26 06:32:42 -0400

    * fix(package.json): updated version to '1.1.16' * style(index.js): added cursor hiding

--- src/index.js
+++ src/index.js
@@ -17,6 +17,7 @@ const MagnifierWrapper = ({
         position: 'relative',
         height: '100%',
         width: '100%',
+        cursor: showMagnifier ? 'none' : 'default', // Hide cursor when magnifier is shown
       }}
     >
       <img
